7fd2c2ffd8449e32cdf184cb47b18c0ac5df102e
AlexeyBaranov92 <violcrimas@gmail.com>
2020-05-17 00:50:21 +0300

    Урок 1, Задание 2

--- app/src/main/java/ru/example/androidlbaranov/SplashScreenActivity.java
+++ app/src/main/java/ru/example/androidlbaranov/SplashScreenActivity.java
@@ -3,13 +3,12 @@ package ru.example.androidlbaranov;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.os.Handler;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashScreenActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash_screen);
     }
 }
